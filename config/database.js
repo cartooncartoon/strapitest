443769dea2e4dae15e978be2ac522160d32e6643
cartooncartoon <46901964+cartooncartoon@users.noreply.github.com>
2020-12-07 18:46:10 -0500

    Update database config

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         "client": "postgres",
-        "host": "${process.env.DATABASE_HOST}",
+        "host": "ec2-54-225-254-115.compute-1.amazonaws.com",
         "port": 5432,
-        "database": "${process.env.DATABASE_NAME}",
-        "username": "${process.env.DATABASE_USERNAME}",
-        "password": "${process.env.DATABASE_PASSWORD}",
+        "database": "ddhrsvgtkfh0ql",
+        "username": "jucnuxcejjqslq",
+        "password": "bc21d253159c6e43b19e135c7c7eeeb97e5719fe9576b91d9b645bb018d5e7e0",
         "ssl": { "rejectUnauthorized": false }
       },
       options: {
